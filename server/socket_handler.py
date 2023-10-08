65e899fae8cca390431714d3e9169f996acf387d
Yosefi Kroytoro <81361291+sefi0609@users.noreply.github.com>
2023-10-08 19:20:56 +0300

    Update socket_handler.py

--- server/socket_handler.py
+++ server/socket_handler.py
@@ -4,18 +4,20 @@ PACKET_SIZE = 1024
 HOST = '127.0.0.1'  # can be '' as well
 
 
-# SocketHandler class - handel the server and client sockets
 class SocketHandler:
+    """ SocketHandler class - handel the server and client sockets """
     def __init__(self, port):
         self.host = HOST
         self.port = port
         self.client_sock = socket.socket()
         self.client_addr = ''
 
-    # create server socket and wait for client connection,
-    # save to class client socket and address,
-    # after the connection is established - no need to save server socket, just close it
     def wait_for_client_socket(self):
+        """
+        Create server socket and wait for client connection,
+        save to class client socket and address,
+        after the connection is established - no need to save server socket, just close it
+        """
         try:
             with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
                 sock.bind((self.host, self.port))
@@ -25,14 +27,16 @@ class SocketHandler:
             print(f'Wait_for_client_socket: {e}')
             self.close_socket()
 
-    # close the client's socket
     def close_socket(self):
+        """ Close the client's socket """
         print(f'Closing socket, client at address: {self.client_addr}')
         self.client_sock.close()
 
-    # send a packet of length PACKET_SIZE to the client,
-    # if the data to be sent is smaller than PACKET_SIZE ped it with zeros
     def send_to_client(self, massage):
+        """
+        send a packet of length PACKET_SIZE to the client,
+        if the data to be sent is smaller than PACKET_SIZE ped it with zeros
+        """
         # make sure massage is size PACKET_SIZE
         bytes_size = len(massage)
         if bytes_size < PACKET_SIZE:
@@ -42,8 +46,8 @@ class SocketHandler:
         except Exception as e:
             print(f'send_to_client: {e}')
 
-    # receive a packet from client of PACKET_SIZE length
     def receive_from_client(self):
+        """ receive a packet from client of PACKET_SIZE length """
         try:
             return self.client_sock.recv(PACKET_SIZE)
         except Exception as e:
